c11811d10cabb1df5678d2bb57b6f2bc22a4cf08
Cristian Toro <cristiantoronavarro.fax@gmail.com>
2024-06-07 17:51:29 -0500

    Updata yaml file v3

--- utils/setter.py
+++ utils/setter.py
@@ -27,5 +27,5 @@ if __name__ == "__main__":
     with open('aws_access_key_id.txt', 'w') as f:
         f.write(aws_access_key_id)
 
-    with open('aws_access_key_id.txt', 'w') as f:
+    with open('aws_secret_key_id.txt', 'w') as f:
         f.write(aws_secret_id)
